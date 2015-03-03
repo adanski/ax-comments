8cd00ec2e3ba651248f346a8fe12078d1d9b2225
Joona Tykkyläinen <joona@viima.com>
2015-03-03 16:05:11 +0200

    Close button fix for mobile

--- tests/spec/MainSpec.js
+++ tests/spec/MainSpec.js
@@ -243,7 +243,7 @@ describe('Basic features', function() {
                 expect(replyField.find('.reply-to-badge').length).toBe(0);
 
                 // Check that the field is last child
-                var lastChild = mostPopularComment.children().last().children().last();
+                var lastChild = mostPopularComment.find('.child-comments').children().last();
                 expect(lastChild[0]).toBe(replyField[0]);
             }); 
 
@@ -264,7 +264,7 @@ describe('Basic features', function() {
                 expect(replyField.find('.reply-to-badge').val()).toBe('@Bryan Connery');
 
                 // Check that the field is last child
-                var lastChild = mostPopularComment.children().last().children().last()
+                var lastChild = mostPopularComment.find('.child-comments').children().last();
                 expect(lastChild[0]).toBe(replyField[0]);
             });
 
