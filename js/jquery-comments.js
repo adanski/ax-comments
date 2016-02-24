6aecb95aff43a97bcbbc7407bf355d0b0211364e
Joona Tykkyläinen <joona@viima.com>
2016-02-24 11:46:12 +0200

    1.1.1

--- js/jquery-comments.js
+++ js/jquery-comments.js
@@ -1,4 +1,4 @@
-//     jquery-comments.js 1.1.0
+//     jquery-comments.js 1.1.1
 
 //     (c) 2016 Joona Tykkyläinen, Viima Solutions Oy
 //     jquery-comments may be freely distributed under the MIT license.
@@ -620,13 +620,13 @@
                     }
 
                     if(pointsOfB != pointsOfA) {
-                        return pointsOfB - pointsOfA > 0;
+                        return pointsOfB - pointsOfA;
 
                     } else {
                         // Return newer if popularity is the same
                         var createdA = new Date(commentA.created).getTime();
                         var createdB = new Date(commentB.created).getTime();
-                        return createdB - createdA > 0;
+                        return createdB - createdA;
                     }
                 });
 
@@ -636,9 +636,9 @@
                     var createdA = new Date(commentA.created).getTime();
                     var createdB = new Date(commentB.created).getTime();
                     if(sortKey == 'oldest') {
-                        return createdB - createdA <= 0;
+                        return createdA - createdB;
                     } else {
-                        return createdB - createdA > 0;
+                        return createdB - createdA;
                     }
                 });
             }
